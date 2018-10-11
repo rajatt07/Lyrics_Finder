e3e3ed10732f6f0ad1a0a87b6aa6170ae1be97b1
Rajat Singh <rajattsingh218@gmail.com>
2018-10-11 15:57:04 +0530

    Delete Navbar.js

--- Navbar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-const Navbar = () => {
-return(
-<nav className= "navbar navbar-dark bg-dark 5-mb">
-<span className=" navbar-brand mb-0 h1 mx-auto">LyricsFinder </span>
-
-</nav>);
-}
-export default Navbar;
